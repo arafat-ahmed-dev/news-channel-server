cf2f9300f236e45d755544618af56536f71af2ee
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-08-28 20:02:33 +0600

    Refactor healthcheck controller to use asyncHandler for improved error handling and add async-handler utility

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,19 +1,10 @@
 import { ApiResponse } from '../utils/api-response.js';
-import { ApiError } from '../utils/api-error.js';
+import { asyncHandler } from '../utils/async-handler.js';
 
-const healthcheck = (req, res) => {
-  try {
-    res
-      .status(200)
-      .json(new ApiResponse(200, { message: 'Service is up and running'
-      }));
-  } catch (error) {
-    res.status(500).json(
-      new ApiError(500, {
-        message: 'Internal Server Error checked by Health Checker',
-      }),
-    );
-  }
-};
+const healthcheck = asyncHandler((req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { message: 'Service is up and running' }));
+});
 
 export { healthcheck };
